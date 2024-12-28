12b6f982508f181103a85d91d4266659c638b5e5
Abdur Razzak <arsduet2122@gmail.com>
2024-12-28 23:28:20 +0600

    Update SHORTING.asm

--- SHORTING.asm
+++ SHORTING.asm
@@ -1,91 +1,83 @@
-ORG 100H
-.DATA                      
-A DB 0,3,6,1,5,2,4,7,9,8                    
-I DB ?
-J DB ?             
-.CODE 
-MAIN PROC
-                           
-    MOV AX, @DATA
-    MOV DS, AX
+.DATA
+ A DB 7,2,1,8,9,3,6,5
+ .CODE
+ MAIN PROC
+    MOV AX,@DATA
+    MOV DS,AX
     
-    
-    MOV CX,10
+    MOV CX,8
     MOV SI,0
-    CONVERT_TO_DIGIT:
-        
-        OR A[SI],48
-        INC SI
-        LOOP CONVERT_TO_DIGIT 
-             
-    MOV CX,10
-    MOV SI,0    
-        
-    PRINT:
-        
+    
+    DIGIT_DECIMAL:
+       OR A[SI],48
+       INC SI
+       LOOP DIGIT_DECIMAL
+       
+     MOV CX,8
+     MOV SI,0
+     PRINT_BEFORE:
         MOV AH,2
-        
         MOV DL,A[SI]
-        INT 21h
-        INC SI 
-        LOOP PRINT
-    
-    MOV AH,2
-    MOV DL,0Dh
-    INT 21h
-    MOV DL,0Ah
-    INT 21h
-    
-    MOV I,-1 
-    
-    OUTER_LOOP:
-        INC I
-        CMP I,9
-        JE EXIT
-        ; J= I
-        MOV BH,I
-        MOV J,BH
-        INNER_LOOP:
-            
-            CMP J,9
-            JE OUTER_LOOP
-            INC J
-           
-            MOV CL,I
-            XOR CH,CH
-            MOV SI,CX
-            
-            MOV DH, A[SI]   
-            
-           
-            MOV CL,J
-            XOR CH,CH
-            MOV SI,CX
-            
-            MOV DL,A[SI]  
-            CMP DH, DL     
-            JGE SWAP
-            JMP INNER_LOOP
-    SWAP:
-        MOV A[SI],DH    
-        MOV CL,I
-        XOR CH,CH
-        MOV SI,CX
-        MOV A[SI],DL    
-        JMP INNER_LOOP
-    
-    EXIT:
-    MOV CX,10
-    MOV SI,0
-    PRINT2:
-        
-        MOV AH,2
-        
-        MOV DL,A[SI]
-        INT 21h
+        INT 21H 
         INC SI
-        
-        LOOP PRINT2   
-ENDP
+        LOOP PRINT_BEFORE  
+     MOV CX,8
+     MOV SI,0
+     MOV DH,-1
+     OUTER_LOOP:
+       INC DH
+       CMP DH,7
+       JGE EXIT
+       MOV DL,DH
+       INNER_LOOP:
+          
+          CMP DL,7   
+          JGE  OUTER_LOOP
+          INC DL  
+          
+          MOV CL,DL
+          MOV CH,0
+          MOV SI,CX
+          MOV BL,A[SI] 
+          
+          MOV CL,DH
+          MOV CH,0
+          MOV SI,CX
+          MOV BH,A[SI] 
+          CMP BH,BL
+          JGE SWAP
+          JMP INNER_LOOP
+       SWAP:
+         MOV CL,DH
+         MOV CH,0
+         MOV SI,CX
+         MOV A[SI],BL  
+         
+         MOV CL,DL
+         MOV CH,0
+         MOV SI,CX
+         MOV A[SI],BH
+         JMP INNER_LOOP
+       EXIT:
+          MOV AH,2H
+          MOV DL,0AH
+          INT 21H
+          MOV DL,0DH
+          INT 21H  
+          
+       MOV CX,8
+      MOV SI,0
+      PRINT_AFTER:
+
+            MOV AH,2
+            MOV DL,A[SI]
+            INT 21h
+            INC SI
+            LOOP PRINT_AFTER
+                    
+          
+MAIN ENDP
 END MAIN
-RET
+       
+       
+    
