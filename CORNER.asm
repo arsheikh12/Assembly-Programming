55f0440a95ba248891c30acc1ffdc9268713e4fa
Abdur Razzak <arsduet2122@gmail.com>
2024-12-28 20:45:31 +0600

    Update CORNER.asm

--- CORNER.asm
+++ CORNER.asm
@@ -1,77 +1,121 @@
 ORG 0100h
-.DATA 
- a db ?
-MX DB 'INPUT VALUE $'                          
-.CODE  
+.DATA
+INPUT DB ?      ; To store the input character
+PROMPT DB 'Input a letter: $'
+NEWLINE DB 0Dh, 0Ah, '$'
+
+.CODE
 MAIN PROC
-    MOV AX, @DATA   
-    MOV DS, AX
+    MOV AX,@DATA
+    MOV DS,AX
     
-    MOV AH,9      ;DISPLAY MX
-    LEA DX,MX
-    INT 21H 
-    ; Input a character
-    MOV AH, 01h
-    INT 21h 
-    MOV a, AL
-    MOV BL, AL
-
-    ; Check if lowercase or uppercase and convert to uppercase if necessary
-    CMP BL, 'a'
-    JB PROCESS
-    CMP BL, 'z'
-    JA NOT_LOWER
-    SUB BL, 32        ;Convert lowercase to uppercase
-
-NOT_LOWER:
-PROCESS:
-    ; Print newline
-    MOV AH, 2 
-    MOV DL, 0DH 
+    MOV AH,09H
+    LEA DX,PROMPT
+    INT 21H
+    
+    MOV AH,1H
+    INT 21H
+    MOV INPUT,AL 
+    
+    MOV AH,2
+    MOV DL,0Ah
     INT 21h
-    MOV DL, 0AH 
+    MOV DL,0Dh
     INT 21h
+ 
+    MOV AL,INPUT
+    CMP AL,'a' 
+    JB UPPERCASE_INPUT
+    CMP AL,'z'
+    JA INVALID_INPUT
+    JMP PROCESS_INPUT_LOWER
+    
+    UPPERCASE_INPUT:
+       CMP AL,'A' 
+       JB  INVALID_INPUT 
+       CMP AL,'Z'
+       JA INVALID_INPUT
+       JMP PROCESS_INPUT_UPPER
+       
+    PROCESS_INPUT_LOWER:
+        MOV BL,INPUT
+        MOV CX,5
+        FORWARD_LOOP_LOWER:
+           INC BL
+           CMP BL,'z'
+           JLE STORE_FORWARD
+           MOV BL,'a'
+           STORE_FORWARD:
+              MOV AL,BL
+              SUB AL,32
+              MOV DL,AL
+              MOV AH,2H
+              INT 21H
+              LOOP FORWARD_LOOP_LOWER 
+           MOV AH,09H
+           LEA DX,NEWLINE
+           INT 21H 
+         MOV CX,5
+         mov bl,input
+         BACKWARD_LOOP_LOWER:
+           DEC BL
+           CMP BL,'a'
+           JGE STORE_BACKWARD
+           MOV BL,'z'
+           STORE_BACKWARD:
+              MOV AL,BL
+              SUB AL,32
+              MOV DL,AL
+              MOV AH,2H
+              INT 21H
+              LOOP BACKWARD_LOOP_LOWER 
+           MOV AH,9
+           LEA DX,NEWLINE
+           INT 21H
+           JMP EXIT 
+           
+      PROCESS_INPUT_UPPER:
+        MOV BL,INPUT
+        MOV CX,5
+        FORWARD_LOOP_UPPER:
+           INC BL
+           CMP BL,'Z'
+           JLE STORE_FORWARD_UPPER
+           MOV BL,'A'
+           STORE_FORWARD_UPPER:
+              MOV AL,BL
+              ADD AL,32
+              MOV DL,AL
+              MOV AH,2H
+              INT 21H
+              LOOP FORWARD_LOOP_UPPER 
+           MOV AH,09H
+           LEA DX,NEWLINE
+           INT 21H 
+         MOV CX,5
+         mov bl,input
+         BACKWARD_LOOP_UPPER:
+           DEC BL
+           CMP BL,'A'
+           JGE STORE_BACKWARD_UPPER
+           MOV BL,'Z'
+           STORE_BACKWARD_UPPER:
+              MOV AL,BL
+              ADD AL,32
+              MOV DL,AL
+              MOV AH,2H
+              INT 21H
+              LOOP BACKWARD_LOOP_UPPER 
+           MOV AH,9
+           LEA DX,NEWLINE
+           INT 21H
+           JMP EXIT
+           
+        
+INVALID_INPUT:
 
-    ; Forward print (5 characters)
-    MOV CX, 5
-FORWARD_LOOP:
-    CMP BL, 'Z'
-    JLE NO_RESET_FORWARD
-    MOV BL,'A'
-NO_RESET_FORWARD:
-    INC BL
-    MOV DL, BL
-    MOV AH, 2
+    MOV AH, 4Ch
     INT 21h
-    LOOP FORWARD_LOOP
-
-    ; Print newline
-    MOV DL, 0DH 
-    MOV AH, 2 
-    INT 21h
-    MOV DL, 0AH 
-    INT 21h
-
-    ; Backward print (5 characters)
-    MOV BL, a
-    CMP BL, 'A'
-    JB ADJUST_BACK
-    CMP BL, 'Z'
-    JA ADJUST_BACK
-    ADD BL, 32        ; Convert uppercase to lowercase if needed
-
-ADJUST_BACK:
-    MOV CX, 5
-BACKWARD_LOOP:
-    CMP BL, 'A'
-    JGE NO_RESET_BACKWARD
-    MOV BL, 'Z' + 1
-NO_RESET_BACKWARD:
-    DEC BL
-    MOV DL, BL
-    MOV AH, 2
-    INT 21h
-    LOOP BACKWARD_LOOP
 
 EXIT:
     MOV AH, 4Ch
